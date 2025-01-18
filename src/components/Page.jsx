39ff0ca03022941fdf1141b246da6cf41ad2bbe4
sham <sham@gmail.com>
2025-01-18 10:23:45 +0530

    updated

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Pagecenter from './Pagecenter'
 
 const Page = () => {
   return (
     <div>
-        <div className='w-[50%] h-[50%] bg-red-500'>
+        <div className='w-[50%] h-[50%] flex items-center justify-center bg-yellow-500'>
             <Pagecenter />
         </div>
     </div>
