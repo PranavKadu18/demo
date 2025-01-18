54f74b7d42d9ec061155201c7a79a6cd4c1f9364
sham <sham@gmail.com>
2025-01-18 10:12:17 +0530

    demo

---
+++ src/components/Page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <div className='w-[50%] h-[50%] bg-red-500'>
+            <Pagecenter />
+        </div>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/components/Pagecenter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pagecenter = () => {
+  return (
+    <div className='w-[100%] h-[100%] bg-blue-500'>Pagecenter</div>
+  )
+}
+
+export default Pagecenter
